42893ad5568b9c63f1276da166c494b59d891bc1
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2021-12-18 15:22:02 +0530

    added docs for exception classes

--- include/exception.hpp
+++ include/exception.hpp
@@ -2,11 +2,23 @@
 
 #include <string>
 
+/**
+ * An exception with error message.
+ *
+ */
 class Exception {
    private:
+    /// The error message.
     std::string message;
 
    public:
+    // parameterized ctor
     Exception(std::string message);
+
+    /**
+     * Get the error message for details about exception.
+     *
+     * @return std::string The error message.
+     */
     std::string getMessage();
 };
--- include/no_update_history_found_exception.hpp
+++ include/no_update_history_found_exception.hpp
@@ -2,10 +2,18 @@
 
 #include "exception.hpp"
 
+/**
+ * The exception for situation when No value update history exists for given key and timestamp.
+ */
 class NoUpdateHistoryFoundException : public Exception {
    private:
    public:
+    /// The default error message.
     static const std::string DEFAULT_MESSAGE;
+
+    /// Default ctor, uses DEFAULT_MESSAGE as error message.
     NoUpdateHistoryFoundException();
+
+    /// Ctor accepting custom error message.
     NoUpdateHistoryFoundException(std::string message);
 };
